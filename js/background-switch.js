0a58be2eb8a29a47a45f715820c4d72a03ced3db
Matan Gover <matangover@gmail.com>
2017-02-03 01:46:47 +0200

    Make background fade out faster.

--- js/background-switch.js
+++ js/background-switch.js
@@ -20,6 +20,6 @@ $(function() {
     backgroundToShow.css("zIndex", 0);
     previousBackground.css("zIndex", 1);
     backgroundToShow.show();
-    previousBackground.fadeOut("100");
+    previousBackground.fadeOut(300);
   })
 })
